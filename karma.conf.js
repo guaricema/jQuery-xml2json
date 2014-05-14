8bd0274f300a4aa33f901397536cd0c282a88782
sergeyt <stodyshev@gmail.com>
2014-05-14 18:01:23 +0700

    fixing build

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'lib/jquery/jquery.js',
+			'lib/jquery/dist/jquery.js',
 			'node_modules/expect.js/index.js',
 			'src/*.js',
 			'test/*.coffee'
