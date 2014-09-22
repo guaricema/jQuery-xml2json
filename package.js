194eedf2f4c6b38c710db26005ea9e222ad5544f
sergeyt <stodyshev@gmail.com>
2014-09-22 22:20:50 +0700

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "sergeyt:jquery-xml2json",
   summary: "jQuery plugin to convert XML to JSON.",
   git: "https://github.com/sergeyt/jquery-xml2json",
-  version: "0.0.7"
+  version: "0.0.8"
 });
 
 Package.onUse(function(api) {
